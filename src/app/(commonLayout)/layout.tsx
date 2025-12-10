100b3eba2c89d04e9136cd8a9bca2a3871456f2f
sultanmahmud07 <asadkhan01862@gmail.com>
2025-12-10 13:16:13 +0600

    Add dynamic route

--- src/app/(commonLayout)/layout.tsx
+++ src/app/(commonLayout)/layout.tsx
@@ -1,6 +1,6 @@
 import Footer from "@/components/shared/Footer";
 import NavbarWrapper from "@/components/shared/Navbar/NavbarWrapper";
-
+export const dynamic = "force-dynamic";
 const CommonLayout = ({ children }: { children: React.ReactNode }) => {
     return (
         <>
